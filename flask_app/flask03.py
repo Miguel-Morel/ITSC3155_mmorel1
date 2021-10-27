4b12cb41b88b9e90399deb86887436ed4dfd66b7
mmorel1 <89816412+mmorel1@users.noreply.github.com>
2021-10-26 22:32:01 -0400

    flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -22,7 +22,7 @@ def index():
 def get_notes():
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-2-2020'}
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
     return render_template('notes.html', notes = notes)
 
@@ -31,9 +31,14 @@ def get_notes():
 def get_note(note_id):
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-2-2020'}
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
     return render_template('note.html', note = notes[int(note_id)])
+
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
+    return render_template('new.html', user=a_user)
 
 
 
