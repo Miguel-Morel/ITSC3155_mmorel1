4522bc7f1246f6bbd877cb8fa9ab5abffa9f9c99
mmorel1 <89816412+mmorel1@users.noreply.github.com>
2021-11-02 21:15:45 -0400

    flask app #4 - added db connection to app

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -1,7 +1,8 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os                 # os is used to get environment variables IP & PORT
+import os
+from database import db                 # os is used to get environment variables IP & PORT
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -9,7 +10,16 @@ from flask.helpers import url_for
 from flask import redirect, url_for
 from werkzeug.utils import redirect # Flask is the web app that we will customize
 
-app = Flask(__name__)     # create an app   
+app = Flask(__name__)     # create an app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
